f69150fdce31f349bf42539ce446b431dedb8aa4
Onur Gumus <onur@outlook.com.tr>
2024-10-11 09:55:10 +0200

    Serializer

---
+++ src/FCQRS.Serialization/Library.fs
@@ -0,0 +1,26 @@
+﻿module FCQRS.Serialization
+
+
+open System
+open System.Text.Json
+open System.Text.Json.Serialization
+
+let jsonOptions =
+    JsonFSharpOptions
+        .Default()
+        .WithSkippableOptionFields()
+        .WithUnionInternalTag()
+        .WithUnionNamedFields()
+        .WithUnionFieldNamesFromTypes()
+        .WithUnionUnwrapSingleCaseUnions(false)
+        .ToJsonSerializerOptions()
+
+let encode obj = JsonSerializer.Serialize(obj, jsonOptions)
+
+let decode<'T> (json: string) =
+            JsonSerializer.Deserialize<'T>(json, jsonOptions)
+        
+let encodeToBytes obj = JsonSerializer.SerializeToUtf8Bytes(obj, jsonOptions)
+
+let decodeFromBytes<'T> (json: byte array) =
+            JsonSerializer.Deserialize<'T>(json, jsonOptions)
