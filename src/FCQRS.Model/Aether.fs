aa4bd863714fe9a1c0d3a0045157db40880520d0
Onur Gumus <onur@outlook.com.tr>
2025-02-01 10:30:18 +0100

    other optics

--- src/FCQRS.Model/Aether.fs
+++ src/FCQRS.Model/Aether.fs
@@ -477,7 +477,7 @@ module Lens =
     let toAsyncValidated (lens: ValidatedLens<'a, 'b, 'e>) : AsyncValidatedLens<'a, 'b, 'e> =
         let (get, set) = lens
         get, (fun b a -> async { return set b a })
-        
+
 [<RequireQualifiedAccess>]
 module Prism =
 
@@ -489,14 +489,149 @@ module Prism =
         let (get, set) = prism
         get, (fun b a -> Ok(set b a))
 
+/// Various optics implemented for common types such as tuples,
+/// lists and maps, along with an identity lens.
 [<AutoOpen>]
 module Optics =
 
+    // Lens for the identity function (does not change the focus of operation).
     let id_: Lens<'a, 'a> = (fun x -> x), (fun x _ -> x)
 
+    /// Isomorphism between a boxed and unboxed type.
+    let box_<'a> : Isomorphism<objnull, 'a> = unbox<'a>, box
+
+    /// Lens to the first item of a tuple.
     let fst_: Lens<('a * 'b), 'a> = fst, (fun a t -> a, snd t)
 
+    /// Lens to the second item of a tuple.
     let snd_: Lens<('a * 'b), 'b> = snd, (fun b t -> fst t, b)
+
+    [<RequireQualifiedAccess>]
+    module Array =
+
+        /// Isomorphism to an list.
+        let list_: Isomorphism<'v[], 'v list> = Array.toList, Array.ofList
+
+    [<RequireQualifiedAccess>]
+    module Choice =
+
+        /// Prism to Choice1Of2.
+        let choice1Of2_: Prism<Choice<_, _>, _> =
+            (fun x ->
+                match x with
+                | Choice1Of2 v -> Some v
+                | _ -> None),
+            (fun v x ->
+                match x with
+                | Choice1Of2 _ -> Choice1Of2 v
+                | _ -> x)
+
+        /// Prism to Choice2Of2.
+        let choice2Of2_: Prism<Choice<_, _>, _> =
+            (fun x ->
+                match x with
+                | Choice2Of2 v -> Some v
+                | _ -> None),
+            (fun v x ->
+                match x with
+                | Choice2Of2 _ -> Choice2Of2 v
+                | _ -> x)
+
+    [<RequireQualifiedAccess>]
+    module Result =
+
+        /// Prism to Ok.
+        let ok_: Prism<Result<_, _>, _> =
+            (fun x ->
+                match x with
+                | Ok v -> Some v
+                | _ -> None),
+            (fun v x ->
+                match x with
+                | Ok _ -> Ok v
+                | _ -> x)
+
+        /// Prism to Error.
+        let error_: Prism<Result<_, _>, _> =
+            (fun x ->
+                match x with
+                | Error v -> Some v
+                | _ -> None),
+            (fun v x ->
+                match x with
+                | Error _ -> Error v
+                | _ -> x)
+
+    [<RequireQualifiedAccess>]
+    module List =
+
+        /// Prism to the head of a list.
+        let head_: Prism<'v list, 'v> =
+            (function
+            | h :: _ -> Some h
+            | _ -> None),
+            (fun v ->
+                function
+                | _ :: t -> v :: t
+                | l -> l)
+
+        /// Prism to an indexed position in a list.
+        let pos_ (i: int) : Prism<'v list, 'v> =
+#if NET35
+            (function
+            | l when List.length l > i -> Some(List.nth l i)
+#else
+            (function
+            | l when List.length l > i -> Some(List.item i l)
+#endif
+            | _ -> None),
+            (fun v l -> List.mapi (fun i' x -> if i = i' then v else x) l)
+
+        /// Prism to the tail of a list.
+        let tail_: Prism<'v list, 'v list> =
+            (function
+            | _ :: t -> Some t
+            | _ -> None),
+            (fun t ->
+                function
+                | h :: _ -> h :: t
+                | [] -> [])
+
+        /// Isomorphism to an array.
+        let array_: Isomorphism<'v list, 'v[]> = List.toArray, List.ofArray
+
+    [<RequireQualifiedAccess>]
+    module Map =
+
+        /// Prism to a value associated with a key in a map.
+        let key_ (k: 'k) : Prism<Map<'k, 'v>, 'v> =
+            Map.tryFind k, (fun v x -> if Map.containsKey k x then Map.add k v x else x)
+
+        /// Lens to a value option associated with a key in a map.
+        let value_ (k: 'k) : Lens<Map<'k, 'v>, 'v option> =
+            Map.tryFind k,
+            (fun v x ->
+                match v with
+                | Some v -> Map.add k v x
+                | _ -> Map.remove k x)
+
+        /// Weak Isomorphism to an array of key-value pairs.
+        let array_: Isomorphism<Map<'k, 'v>, ('k * 'v)[]> = Map.toArray, Map.ofArray
+
+        /// Weak Isomorphism to a list of key-value pairs.
+        let list_: Isomorphism<Map<'k, 'v>, ('k * 'v) list> = Map.toList, Map.ofList
+
+    [<RequireQualifiedAccess>]
+    module Option =
+
+        /// Prism to the value in an Option.
+        let value_: Prism<'v option, 'v> =
+            id,
+            (fun v ->
+                function
+                | Some _ -> Some v
+                | None -> None)
+
 
 module Operators =
 
