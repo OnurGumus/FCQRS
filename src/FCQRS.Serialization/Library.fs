71b0b5521bcd43d46516f19cd42951a246f64c1e
Onur Gumus <onur@outlook.com.tr>
2025-02-09 10:03:57 +0100

    Remove fieldnames from types

--- src/FCQRS.Serialization/Library.fs
+++ src/FCQRS.Serialization/Library.fs
@@ -11,7 +11,7 @@ let jsonOptions =
         .WithSkippableOptionFields()
         .WithUnionInternalTag()
         .WithUnionNamedFields()
-        .WithUnionFieldNamesFromTypes()
+        //.WithUnionFieldNamesFromTypes()
         .WithUnionUnwrapSingleCaseUnions(false)
         .ToJsonSerializerOptions()
 
